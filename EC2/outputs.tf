54cc7ffe694c8bd8729d2b9ab1db787c8573f5f1
Ubuntu <ubuntu@ip-172-31-24-93.eu-north-1.compute.internal>
2025-10-30 08:16:15 +0000

    Day 2 commit

---
+++ EC2/outputs.tf
@@ -0,0 +1,12 @@
+
+output "ec_public_ip" {
+	value = aws_instance.my_instance.public_ip
+}
+
+output "ec_public_dns" {
+        value = aws_instance.my_instance.public_dns
+}
+
+output "ec_private_ip" {
+        value = aws_instance.my_instance.private_ip
+}
